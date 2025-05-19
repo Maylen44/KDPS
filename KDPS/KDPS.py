29f792a3a0903a01c47befb954080f06333e87d2
Maylen44 <belz1993@bk.ru>
2025-05-19 09:28:51 +0200

    1st Labyrinth exercise added

--- KDPS/KDPS.py
+++ KDPS/KDPS.py
@@ -1,42 +1,16 @@
-#hello.py
+#lab01.py
 
-'''
-Mehrzeilige Kommentare mit 3 '
-'''
+string1 = "-" * 33
+string2 = "-" * 12 + "LABYRINTH" + "-" * 12
 
-#Einzeilige Kommentare mit #
-#Kein Untershied zwischen ' und "
+print(string1)
+print(string2)
+print(string1)
 
-'''
-Python Keywords
-False None True
-and as assert async await break class continue
-def del elif else except finally for from
-global if import in is lambda nonlocal not
-or pass raise return try while with yield
-help() -> keywords
-import keyword
-print(keyword.kwlist)
-print(keyword.softkwlist)
-'''
+#oder mit 3 "
 
-
-a = 24
-b = 43.22
-c = 2 + 43
-d = "Hello"
-e = True
-f = None
-
-print(a, b, c, d, e, f)
-print(type(a), type(b), type(c), type(d), type(e), type(f))
-
-a = float(a)
-b = complex(24, 52)
-c = int(-4.11)
-d = str(e)
-e = bool("Hello")
-f = bool()
-g = bool("False")
-
-print(a, b, c, d, e, f, g)
+print("""\
+---------------------------------
+------------LABYRINTH------------
+---------------------------------\
+""")
