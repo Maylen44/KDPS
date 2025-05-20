8183731d9c11e13f0bf9534c718ff98a73b89650
Maylen44 <belz1993@bk.ru>
2025-05-20 14:44:06 +0200

    Lab02 Exercise done

--- KDPS/KDPS.py
+++ KDPS/KDPS.py
@@ -1,16 +1,58 @@
-#lab01.py
+#lab02.py
 
-string1 = "-" * 33
-string2 = "-" * 12 + "LABYRINTH" + "-" * 12
-
-print(string1)
-print(string2)
-print(string1)
-
-#oder mit 3 "
-
-print("""\
----------------------------------
+welcomeText = """---------------------------------
 ------------LABYRINTH------------
----------------------------------\
-""")
+---------------------------------"""
+actionText = "Ihr Befehl (L-Links, R-Rechts, G-Gehen, X-Ende): "
+roomID = 0
+directionID = 0
+rooms = ["Kuche", "Wohnzimmer", "Schlafzimmer", "Koridor"]
+directions = ["Norden", "Osten", "Suden", "Westen"]
+
+print(welcomeText)
+
+while True:
+    print("Sie befinden sich in Raum: '%s'." % rooms[roomID])
+    print("Ihr Blick ist nach '%s' gerichtet." % directions[directionID])
+    playerInput = input(actionText)
+    
+    if playerInput == 'L' or playerInput == 'l':
+        if directionID == 0:
+            directionID = 3
+        else:
+            directionID -= 1
+    elif playerInput == 'R' or playerInput == 'r':
+        if directionID == 3:
+            directionID = 0
+        else:
+            directionID += 1
+    elif playerInput == 'G' or playerInput == 'g':
+        match (roomID, directionID):
+            case (0, 0):
+                roomID = 3
+            case (3, 2):
+                roomID = 0
+            case (3, 1):
+                roomID = 1
+            case (1, 3):
+                roomID = 3
+            case (3, 3):
+                roomID = 2
+            case (2, 1):
+                roomID = 3
+    elif playerInput == 'X' or playerInput == 'x':
+        print("Bye Bye!")
+        break
+    else:
+        print("Falsche Eingabe. Probiere nochmal!")
+
+
+
+
+
+
+
+
+
+
+
